66e006974200cda61f61c62afb94ef6a9d75ec9a
haojingshao <haojingshao@gmail.com>
2020-01-23 17:49:23 +1000

    first code

---
+++ githubPublication/example/MyTest.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+module load biopython
+module load samtools
+module load bedtools
+module load hisat2
+dir=`pwd`
+echo "fastq1 fastq2 Run line id" > list
+echo "$dir/1.1.fq $dir/1.2.fq Run_1 33 1" >> list
+echo "$dir/2.1.fq $dir/2.2.fq Run_1 33 2" >> list
+../pipeline.pl --vcf1=$dir/P1.vcf --vcf2=$dir/P2.vcf --ref=$dir/ref.fa --fastq=$dir/list --vcf1Name=P1 --vcf2Name=P2 --outDir=$dir/TestOut --cpu=8 --path=/home/uqhshao/30days/FruitFly/pipeline/githubPublication --exon=$dir/exon.bed
